9d5b2766a9ae253926b039d8742951761b1b6494
romikkikvadze777-beep <romik.kikvadze777@gmail.com>
2025-12-23 09:44:15 +0300

    first commit

---
+++ BasicDataTypes/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program{
+static void Main()
+        {
+            string[] words = { "apple", "cat", "banana", "dog", "elephant" };
+            var result = words.Where(w => w.Length > 3).ToArray();
+            Console.WriteLine($"Task 7: {string.Join(", ", result)}");
+        }
+}
